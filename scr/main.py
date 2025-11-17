44d0f2cafe841117d2e79d0b7adf8989cf459185
Забродина Мария <z.mashenka25@mail.ru>
2025-11-17 19:43:58 +0300

    Добавлен вывод для текстового вывода

--- scr/main.py
+++ scr/main.py
@@ -62,21 +62,92 @@ def insert_sample_data(db: str = "users.db"):
         ]
         cursor.executemany("INSERT INTO Students(Name) VALUES (?)", students)
         print("Добавлен студент.")
-
-    cursor.execute("SELECT COUNT(*) FROM Books")
     if cursor.fetchone()[0] == 0:
-        books = [
-            ("Война и мир", 1),
-            ("Анна Каренина", 1),
-            ("Идиот", 2),
-            ("Преступление и наказание", 2),
-            ("Братья Карамазовы", 2),
-            ("Вишневый сад", 3),
-            ("Чайка", 3),
-            ("Отцы и дети", 4)
+        Numbergroop = [
+            ("25-ИВТ-2-1",),
+            ("25-ИСТ-2")
         ]
-        cursor.executemany("INSERT INTO Books (Title, Author_ID) VALUES (?, ?)", books)
-        print("Добавлены книги.")
+        cursor.executemany("INSERT INTO Students(Numbergroop) VALUES (?)",Numbergroop)
+        print("Добавлен номер группы. ")
+
+
+    cursor.execute("SELECT COUNT(*) FROM Room")
+    if cursor.fetchone()[0] == 0:
+        students = [
+            ("Мария Забродина",),
+            ("Дарья Демидова")
+        ]
+        cursor.executemany("INSERT INTO Room(Name) VALUES (?)", students)
+        print("Добавлен студент.")
+    if cursor.fetchone()[0] == 0:
+        Size = [
+            ("3 человека",),
+            ("2 человека")
+        ]
+        cursor.executemany("INSERT INTO Room(Size) VALUES (?)", Size)
+        print("Добавлен размер комнаты проживания.")
+    if cursor.fetchone()[0] == 0:
+        Status = [
+            ("Частично занята",),
+            ("Занята",),
+            ("Свообдна")
+        ]
+        cursor.executemany("INSERT INTO Room(Status) VALUES (?)", Status)
+        print("Добавлен статус комнаты.")
+    if cursor.fetchone()[0] == 0:
+        id_obsh = [
+            ("1")
+        ]
+        cursor.executemany("INSERT INTO Room(id_obsh) VALUES (?)", id_obsh)
+        print("Добавлен ID общежития.")
+
+    cursor.execute("SELECT COUNT(*) FROM Student_room")
+    if cursor.fetchone()[0] == 0:
+        id_students = [
+            ("1. Мария Забродина",),
+            ("2. Дарья Демидова")
+        ]
+        cursor.executemany("INSERT INTO Student_room(id_students) VALUES (?)", id_students)
+        print("Добавлен ID студента.")
+    if cursor.fetchone()[0] == 0:
+        id_room = [
+            ("1. 175",),
+            ("2. 176")
+        ]
+        cursor.executemany("INSERT INTO Student_room(id_room) VALUES (?)", id_room)
+        print("Добавлен ID комнаты.")
+
+    cursor.execute("SELECT COUNT(*) FROM House_student")
+    if cursor.fetchone()[0] == 0:
+        Name = [
+            ("Мария Забродина",),
+            ("Дарья Демидова")
+        ]
+        cursor.executemany("INSERT INTO House_student(Name) VALUES (?)", Name)
+        print("Добавлен студент.")
+    if cursor.fetchone()[0] == 0:
+        Numberoom = [
+            ("175",),
+            ("176")
+        ]
+        cursor.executemany("INSERT INTO House_student(Numberoom) VALUES (?)", Numberoom)
+        print("Добавлен номер комнаты.")
+
+    cursor.execute("SELECT COUNT(*) FROM Student_doc")
+    if cursor.fetchone()[0] == 0:
+        Name = [
+            ("Мария Забродина",),
+            ("Дарья Демидова")
+        ]
+        cursor.executemany("INSERT INTO Student_doc(Name) VALUES (?)", Name)
+        print("Добавлен студент.")
+    if cursor.fetchone()[0] == 0:
+        Number = [
+            ("1",),
+            ("2")
+        ]
+        cursor.executemany("INSERT INTO Student_doc(Number) VALUES (?)", Number)
+        print("Добавлен номер.")
 
     conn.commit()
     conn.close()

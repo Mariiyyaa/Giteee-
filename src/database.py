79fecc2953725b2cfffce20b5147ad920c775dfc
Забродина Мария <z.mashenka25@mail.ru>
2025-11-18 10:42:29 +0300

    Добавлен текстовый вывод

--- src/database.py
+++ src/database.py
@@ -11,23 +11,15 @@ def create_tables(db: str = "users.db"):
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS Room (
             ID INTEGER PRIMARY KEY AUTOINCREMENT,
-            Name VARCHAR(50),
+            Name_room VARCHAR(50),
             Size VARCHAR(50),
             Status VARCHAR(50),
-            id_obsh VARCHAR(50)
+            Number_ob VARCHAR(50)
         )
     ''')
 
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS Student_room (
-            ID INTEGER PRIMARY KEY AUTOINCREMENT,
-            id_students VARCHAR(50),
-            id_room VARCHAR(50)
-        )
-    ''')
-
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS House_student (
             ID INTEGER PRIMARY KEY AUTOINCREMENT,
             Name VARCHAR(50),
             Numberoom VARCHAR(50)
@@ -41,14 +33,14 @@ def create_tables(db: str = "users.db"):
             Numbergroop VARCHAR(50)
         )
     ''')
-
     cursor.execute('''
-        CREATE TABLE IF NOT EXISTS Student_doc (
-            ID INTEGER PRIMARY KEY AUTOINCREMENT,
-            Name VARCHAR(50),
-            Number VARCHAR(50)
-        )
-    ''')
+           CREATE TABLE IF NOT EXISTS Starosta (
+               ID INTEGER PRIMARY KEY AUTOINCREMENT,
+               Name starost VARCHAR(50),
+               Zadacha starost VARCHAR(50)
+           )
+       ''')
+
     conn.commit()
     conn.close()
 
@@ -73,38 +65,27 @@ def insert_sample_data(db: str = "users.db"):
             ("176", "0 человек", "Свободна", "1"),
             ("177", "3 человека", "Занята", "1")
         ]
-        cursor.executemany("INSERT INTO Room (Name, Size, Status, id_obsh) VALUES (?, ?, ?, ?)", rooms)
+        cursor.executemany("INSERT INTO Room (Name_room, Size, Status, Number_ob) VALUES (?, ?, ?, ?)", rooms)
         print("Комната добавлена.")
 
 
     cursor.execute("SELECT COUNT(*) FROM Student_room")
     if cursor.fetchone()[0] == 0:
         student_rooms = [
-            ("1", "175"),
-            ("2", "176")
-        ]
-        cursor.executemany("INSERT INTO Student_room (id_students, id_room) VALUES (?, ?)", student_rooms)
-        print("Студент заселен.")
-
-
-    cursor.execute("SELECT COUNT(*) FROM House_student")
-    if cursor.fetchone()[0] == 0:
-        house_students = [
             ("Мария Забродина", "175"),
             ("Дарья Демидова", "176")
         ]
-        cursor.executemany("INSERT INTO House_student (Name, Numberoom) VALUES (?, ?)", house_students)
-        print("Студент заселён.")
+        cursor.executemany("INSERT INTO Student_room (Name, Number room) VALUES (?, ?)", student_rooms)
+        print("Студент заселен.")
 
-
-    cursor.execute("SELECT COUNT(*) FROM Student_doc")
+    cursor.execute("SELECT COUNT(*) FROM Starosta")
     if cursor.fetchone()[0] == 0:
-        student_docs = [
-            ("Мария Забродина", "1"),
-            ("Дарья Демидова", "2")
+        starosta = [
+            ("Дима Дмитриев, обновление данных о проживающих студентах"),
+            ("Иван Иванов, внесение информации о состоянии комнат")
         ]
-        cursor.executemany("INSERT INTO Student_doc (Name, Number) VALUES (?, ?)", student_docs)
-        print("Добавлены номера студентов.")
+        cursor.executemany("INSERT INTO Statosta (Name starost, Zadacha starost) VALUES (?, ?)", starosta)
+        print("Функции старосты определены.")
 
     conn.commit()
     conn.close()
